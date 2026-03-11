132aa1fc6527e9f5b13c26d88e16523d7a5260d3
Lucas Ritzdorf <42657792+LRitzdorf@users.noreply.github.com>
2026-03-10 19:14:52 -0700

    testbench(c): clean up formatting

--- example_tb/core/custom/indirect_jumps.c
+++ example_tb/core/custom/indirect_jumps.c
@@ -6,20 +6,19 @@ static int sub(int a, int b) { return a - b; }
 static int mul(int a, int b) { return a * b; }
 
 typedef int (*binop_t)(int, int);
-
 static volatile binop_t ops[] = { add, sub, mul };
 
 int main(void)
 {
     /* Enable zicfilp */
-    __asm__ volatile("csrw 0x747, %0" :: "r"(1 << 2));
+    __asm__ volatile("csrw mseccfg, %0" :: "r"(1 << 2));
 
-    int r = ((binop_t)ops[0])(10, 3);
-    r = ((binop_t)ops[1])(r, 5);
-    r = ((binop_t)ops[2])(r, 3);
+    int r = (ops[0])(10, 3);
+    r = (ops[1])(r, 5);
+    r = (ops[2])(r, 3);
 
     /* Disable before returning to non-CFI library code */
-    __asm__ volatile("csrw 0x747, %0" :: "r"(0));
+    __asm__ volatile("csrw mseccfg, %0" :: "r"(0));
 
     printf("Result: %d\n", r);
     return EXIT_SUCCESS;
